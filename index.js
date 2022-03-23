7546ca3e59286187ee8464c843e08d550fcdf8a8
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2022-03-23 14:43:59 +0100

    First commit counter

---
+++ index.js
@@ -0,0 +1,17 @@
+//Import
+import fetch from 'node-fetch';
+import express from 'express';
+const app = express();
+
+app.get('/count', async (req, res) => {
+    res.set({
+        'content-type': 'image/png',
+        'cache-control': 'max-age=0, no-cache, no-store, must-revalidate'
+    })
+
+    const img = await ((await fetch('https://counter9.stat.ovh/private/compteurdevisite.php?c=dzct1uqm5lpgwmqn18387dkn26w125w5'))).arrayBuffer();
+
+    res.end(new Uint8Array(img), 'binary');
+});
+
+app.listen(81, () => console.log('Ready!'));
